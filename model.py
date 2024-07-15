96f56225724c45b83d48daf8806cc69565f14d4f
roket1428 <meorhan@protonmail.com>
2024-07-15 23:20:15 +0300

    Fix most ml backend bugs

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import os, shutil, json, time
+import os, shutil, json, time, subprocess, glob
 
 from typing import List, Dict, Optional
 from label_studio_ml.model import LabelStudioMLBase
@@ -7,6 +7,7 @@ from label_studio_ml.utils import get_image_size, is_skipped
 
 from ultralytics.models import YOLO
 
+TRAINED_MODEL_ = os.path.join(os.getenv("CHECKPOINT_DIR", "./yolobk/data/checkpoints"), "yolov10n_trained_.pt")
 TRAINED_MODEL = os.path.join(os.getenv("CHECKPOINT_DIR", "./yolobk/data/checkpoints"), "yolov10n_trained.pt")
 INITIAL_MODEL = os.path.join(os.getenv("CHECKPOINT_DIR", "./yolobk/data/checkpoints"), "yolov10n.pt")
 BEST_MODEL = "./yolobk/runs/detect/train/weights/best.pt"
@@ -63,9 +64,9 @@ class YOLOv10(LabelStudioMLBase):
         Run prediction on {tasks}
         Received context: {context}
         Project ID: {self.project_id}
-        Label config: {self.label_config}
-        Parsed JSON Label config: {self.parsed_label_config}
         Extra params: {self.extra_params}''')
+        # Label config: {self.label_config}
+        # Parsed JSON Label config: {self.parsed_label_config}
 
         task = tasks[0]
         model = YOLO(self.current_model)
@@ -74,9 +75,17 @@ class YOLOv10(LabelStudioMLBase):
         image_width, image_height = get_image_size(image_path)
         results = model(image_path)
         boxes = results[0].boxes
-        all_scores = []
         output = []
+        max_conf = {}
+        max_conf["minus"] = max_conf["plus"] = 0
         for (x_min, y_min, x_max, y_max), conf, cls in zip((boxes.xyxy.numpy()), boxes.conf.numpy(), boxes.cls.numpy()):
+            current_class = model.names[cls]
+            if not current_class in ["minus", "plus"]:
+                continue
+
+            if conf > max_conf[current_class]:
+                max_conf[current_class] = conf
+
             output.append({
                 "from_name": "label",
                 "to_name": "image",
@@ -84,18 +93,27 @@ class YOLOv10(LabelStudioMLBase):
                 "original_height": image_height,
                 "type": "rectanglelabels",
                 "value": {
-                    "rectanglelabels": [model.names[cls]],
-                    "x": x_min / image_width * 100,
-                    "y": y_min / image_height * 100,
-                    "width": (x_max - x_min) / image_width * 100,
-                    "height": (y_max - y_min) / image_height * 100
+                    "rectanglelabels": [current_class],
+                    "x": float(x_min / image_width * 100),
+                    "y": float(y_min / image_height * 100),
+                    "width": float((x_max - x_min) / image_width * 100),
+                    "height": float((y_max - y_min) / image_height * 100)
                 },
-                "score": conf
+                "score": float(conf)
             })
-            all_scores.append(conf)
+
+        all_scores = []
+        output_processed = []
+        for pred in output:
+            if pred["score"] < max_conf[pred["value"]["rectanglelabels"][0]]:
+                continue
+            else:
+                output_processed.append(pred)
+                all_scores.append(pred["score"])
+
         avg_score = sum(all_scores) / max(len(all_scores), 1)
         predictions = [{
-            "result": output,
+            "result": output_processed,
             "score": avg_score,
             "model_version": self.get("model_version")
         }]
@@ -114,6 +132,14 @@ class YOLOv10(LabelStudioMLBase):
         """
         if (event == "ANNOTATION_CREATED" or event == "ANNOTATION_UPDATED" or event == "START_TRAINING"):
             if (event == "ANNOTATION_CREATED" or event == "ANNOTATION_UPDATED"):
+
+                # check if the current annotation is skipped
+                data_wrapper = {}
+                data_wrapper["annotations"] = [data["annotation"]]
+                if is_skipped(data_wrapper):
+                    return
+
+                # update state
                 if self.for_training > 0:
                     current_images = TRAIN_IMAGES
                     current_labels = TRAIN_LABELS
@@ -131,65 +157,106 @@ class YOLOv10(LabelStudioMLBase):
                     print("updating the annotation")
                     # dont count updates as new annotation
                     self.waiting_annotations -= 1
-                    if not os.path.isfile(os.path.join(current_images, image_name)):
+                    if self.current_target == "train":
+                        self.for_training += 1
                         # if updated annotation isn't in the current set
-                        if self.current_target == "train":
-                            # neutralize training counter and switch the target dirs
-                            self.for_training += 1
+                        if not os.path.isfile(os.path.join(current_images, image_name)):
                             current_images = VAL_IMAGES
                             current_labels = VAL_LABELS
-                        else:
-                            # viceversa
-                            self.for_validation += 1
+                    else:
+                        self.for_validation += 1
+                        if not os.path.isfile(os.path.join(current_images, image_name)):
                             current_images = TRAIN_IMAGES
                             current_labels = TRAIN_LABELS
 
                 # create the dataset at the target
                 self.create_dataset(data, image_path, image_name, current_images, current_labels)
 
+                # update state
                 if self.for_training > 0:
                     self.for_training -= 1
                 else:
                     self.for_validation -= 1
-
                 self.waiting_annotations += 1
 
-            if self.waiting_annotations == 40:
-                print("training started")
-                os.system(f"yolo \
-                    mode=train \
-                    workers={self.workers} \
-                    batch={self.batch_size} \
-                    imgsz={self.imgsz} \
-                    epochs={self.epochs} \
-                    model={self.current_model} \
-                    data={os.path.join(DATASET_DIR, "data.yaml")}")
+                # save state
+                self.dataset_state["waiting_annotations"] = self.waiting_annotations
+                self.dataset_state["current_target"] = self.current_target
+                self.dataset_state["for_training"] = self.for_training
+                self.dataset_state["for_validation"] = self.for_validation
+                self.save_state(self.dataset_state)
 
-                shutil.copyfile(BEST_MODEL, TRAINED_MODEL)
-                shutil.copytree("./yolobk/runs/detect/train", "./yolobk/archive/runs/" + str(int(time.time())))
-                shutil.rmtree("./yolobk/runs/detect/train")
+            if self.waiting_annotations == 80:
+                # check for human errors before training
+                self.fix_human_errors_on_labelling()
+                if os.path.isfile(BEST_MODEL):
+                    shutil.copyfile(BEST_MODEL, TRAINED_MODEL_)
+                    shutil.copytree("./yolobk/runs/detect/train", "./yolobk/archive/runs/" + str(int(time.time())))
+                    shutil.rmtree("./yolobk/runs/detect")
+                print("starting the training")
+                subprocess.Popen(["yolo",
+                                  "mode=train",
+                                  f"workers={self.workers}",
+                                  f"batch={self.batch_size}",
+                                  f"imgsz={self.imgsz}",
+                                  f"epochs={self.epochs}",
+                                  f"model={self.current_model}",
+                                  f"data={os.path.join(DATASET_DIR, "data.yaml")}"])
+                print("training started as an async sub-process")
+                # update the model version
+                model_version = self.get("model_version")
+                self.set("model_version", ".".join(model_version.split(".")[:-1]) + "." + str(int(model_version.split(".")[-1]) + 1))
+
                 # reset state
-                # model_version = self.get("model_version")
-                # self.set("model_version", ".".join(model_version.split(".")[:-1]) + "." + str(int(model_version.split(".")[-1]) + 1))
                 self.current_target = "train"
                 self.waiting_annotations = 0
-                self.for_training = 30
-                self.for_validation = 10
-                print("training finished")
+                self.for_training = 60
+                self.for_validation = 20
+                self.dataset_state["waiting_annotations"] = self.waiting_annotations
+                self.dataset_state["current_target"] = self.current_target
+                self.dataset_state["for_training"] = self.for_training
+                self.dataset_state["for_validation"] = self.for_validation
+                self.save_state(self.dataset_state)
+                print("training probably continues as a subprocess but the training part of the fit function ended")
 
-            self.dataset_state["waiting_annotations"] = self.waiting_annotations
-            self.dataset_state["current_target"] = self.current_target
-            self.dataset_state["for_training"] = self.for_training
-            self.dataset_state["for_validation"] = self.for_validation
-            self.save_state(self.dataset_state)
             print(event)
+            print(data)
             print(self.waiting_annotations)
             print(self.for_training)
             print(self.for_validation)
             print(self.current_target)
 
+    def fix_human_errors_on_labelling(self):
+        """
+        Fix wrong labelling on the human side.
+        Since all images have class names in it we can use that to check incorrectly assingned class names.
+        """
+        plus_labels = glob.glob(os.path.join(TRAIN_LABELS, "p*.txt")) + glob.glob(os.path.join(VAL_LABELS, "p*.txt"))
+        minus_labels = glob.glob(os.path.join(TRAIN_LABELS, "m*.txt")) + glob.glob(os.path.join(VAL_LABELS, "m*.txt"))
+        label_classes = {}
+        label_classes["minus"] = 0
+        label_classes["plus"] = 1
+        for label_path in plus_labels:
+            with open(label_path) as f:
+                label_txt = f.read()
+            label_class = int(label_txt[0])
+            if label_class != label_classes["plus"]:
+                label_txt = str(label_classes["plus"]) + label_txt[1:]
+                with open(label_path, "w") as f:
+                    f.write(label_txt)
+        for label_path in minus_labels:
+            with open(label_path) as f:
+                label_txt = f.read()
+            label_class = int(label_txt[0])
+            if label_class != label_classes["minus"]:
+                label_txt = str(label_classes["minus"]) + label_txt[1:]
+                with open(label_path, "w") as f:
+                    f.write(label_txt)
+
     def create_dataset(self, data, image_path, image_name, images_dir, labels_dir):
         annotation = data["annotation"]
+        if os.path.isfile(os.path.join(labels_dir, image_name[:-4]+'.txt')):
+            os.remove(os.path.join(labels_dir, image_name[:-4]+'.txt'))
         for bbox in annotation['result']:
             bb_width = (bbox['value']['width']) / 100
             bb_height = (bbox['value']['height']) / 100
